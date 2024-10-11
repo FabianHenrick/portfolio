ff9f64fd015208335208e43caf51e5c27d66f69f
FabianHenrick <fabianhenrick@gmail.com>
2024-10-11 09:05:19 -0300

    react-router-dom  implementation 0.8v

--- src/Components/Header/router.tsx
+++ src/Components/Header/router.tsx
@@ -3,12 +3,17 @@ import { Home } from "../../Pages/home";
 import { Expertise } from "../../Pages/expertise";
 import { About } from "../../Pages/about";
 import { Projects } from "../../Pages/projects";
+import { DefaultLayout } from "../layout/defaultLayout";
 
 export function Router() {
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/expertise" element={<Expertise />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/projects" element={<Projects />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/expertise" element={<Expertise />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/Components/layout/defaultLayout/index.tsx
+++ src/Components/layout/defaultLayout/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../../Header";
+import { Header } from "../../Header/index.tsx";
 import { LayoutContainer } from "./styles.ts";
 
 export function DefaultLayout() {
